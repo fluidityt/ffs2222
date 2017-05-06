f0fc7d6f4d3264929dfbf49e42072fd329898cb0
fluidityt <hackbook17@justins-macbook-pro.local>
2017-05-06 06:37:36 -0400

    Made and refaced to Toggler

--- ffs2222/MainMenuScene.swift
+++ ffs2222/MainMenuScene.swift
@@ -39,127 +39,23 @@ final class MainMenuScene: SKScene {
     override init() { super.init() }
   };
   
-  private final class DevModeLabel: SKLabelNode {
-    
-    func devModeOff() {
-      devmode.value = false
-      self.text = "DevMode Off"
-    }
-    
-    func devModeOn() {
-      devmode.value = true
-      self.text = "DevMode On"
-    }
-    
-    init(texter: String) {
-      super.init(fontNamed: "Chalkduster")
-      self.text = "DevMode Off"
-      isUserInteractionEnabled = true
-      position.y -= 100
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-      if devmode.value { devModeOff() } else { devModeOn() }
-    }
-    
-    override init() { super.init() }
-    required init?(coder aDecoder: NSCoder) { fatalError("") }
-    
-  };
-  
-  private final class SpinModeLabel: SKLabelNode {
-    
-    func spinModeOff() {
-      spinning .value = false
-      self.text = "SpinMode Off"
-    }
-    
-    func spinModeOn() {
-      spinning.value = true
-      self.text = "SpinMode On"
-    }
-    
-    init(texter: String) {
-      super.init(fontNamed: "Chalkduster")
-      self.text = "SpinMode Off"
-      isUserInteractionEnabled = true
-      position.y -= 200
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-      if spinning.value { spinModeOff() } else { spinModeOn() }
-    }
-    
-    override init() { super.init() }
-    required init?(coder aDecoder: NSCoder) { fatalError("") }
-  };
-  
-  private final class FadeModeLabel: SKLabelNode {
-    
-    func fadeModeOff() {
-      fademode.value = false
-      self.text = "FadeMode Off"
-    }
-    
-    func fadeModeOn() {
-      fademode.value = true
-      self.text = "FadeMode On"
-    }
-    
-    init(texter: String) {
-      super.init(fontNamed: "Chalkduster")
-      self.text = "FadeMode Off"
-      isUserInteractionEnabled = true
-      position.y -= 300
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-      if fademode.value { fadeModeOff() } else { fadeModeOn() }
-    }
-    
-    override init() { super.init() }
-    required init?(coder aDecoder: NSCoder) { fatalError("") }
-    
-  };
-  
-  private final class FullModeLabel: SKLabelNode {
-    
-    func fullModeOff() {
-      fullmode.value = false
-      self.text = "FullMode Off"
-    }
-    
-    func fullModeOn() {
-      fullmode.value = true
-      self.text = "FullMode On"
-    }
-    
-    init(texter: String) {
-      super.init(fontNamed: "Chalkduster")
-      self.text = "FullMode Off"
-      isUserInteractionEnabled = true
-      position.y -= 400
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-      if fullmode.value { fullModeOff() } else { fullModeOn() }
-    }
-    
-    override init() { super.init() }
-    required init?(coder aDecoder: NSCoder) { fatalError("") }
-    
-  }
+
   
   /// let isplaying = RefBool(false)
   
   private func makeLabels() {
-    addChild(PlayLabel    (texter: ""))
-    addChild(DevModeLabel (texter: ""))
-    addChild(SpinModeLabel(texter: ""))
-    addChild(FadeModeLabel(texter: ""))
-    addChild(FullModeLabel(texter: ""))
     
-    /// addChild(Toggler(labelName: "Play Game", refBool: isplaying))
+    let labels: [SKLabelNode] = [
+      PlayLabel(texter: ""),
+      Toggler(labelName: "DevMode", refBool: devmode),
+      Toggler(labelName: "SpinMode", refBool: spinning),
+      Toggler(labelName: "FadeMode", refBool: fademode),
+      Toggler(labelName: "FullMode", refBool: fullmode)
+    ]
+    offSetLabel(labels, by: 50)
+    addChildren(labels)
+    
+
   }
   
   /**
